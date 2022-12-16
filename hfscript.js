3609da2eed4cb717daabfef0c05aeffdf699671f
Jacob1539 <87676791+Jacob1539@users.noreply.github.com>
2022-12-16 18:41:52 -0500

    Add files via upload

---
+++ hfscript.js
@@ -0,0 +1,16 @@
+function playCorrect(){
+    var correct = new Audio('correct.m4a');
+    correct.play();
+}
+function playIncorrect(){
+    var incorrect = new Audio('incorrect.m4a');
+    incorrect.play();
+}
+function playLong(){
+    var long = new Audio('long.m4a');
+    long.play();
+}
+function playShort(){
+    var short = new Audio('short.m4a');
+    short.play();
+}
